738450a59c3c4aa1f5d8ff129bd0ac31c5180062
Andrei Yeutukhou <andrei.yeutukhou@gmail.com>
2026-03-01 21:42:47 +0300

    first page mobile vp ready

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// custom input reset
 const resetBtn = document.querySelector('.custom-input__reset-btn')
 const customSearchInput = document.querySelector('.custom-input__field')
 const searchResultsForm = document.querySelector('.search-results__form')
@@ -16,3 +17,21 @@ resetBtn.addEventListener('click', () => {
   customSearchInput.focus()
 })
 
+//pagination
+const paginationList = document.querySelectorAll('.pagination__item')
+
+paginationList.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const counterLink = item.querySelector('a.pagination__link.pagination__link--counter')
+
+    if (counterLink) {
+      paginationList.forEach((el) => {
+        el.classList.remove('pagination__item--active')
+      })
+
+      item.classList.add('pagination__item--active')
+    }
+  })
+})
